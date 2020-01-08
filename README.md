a40c2a3acbe324de4698f5216f0b8db1ea7bc102
knowbee <knowbeeinc@gmail.com>
2020-01-08 20:57:40 +0200

    got all clinics

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hackmaps
-Attempt to get useful data from google maps
+
+## setup
+
+```cli
+pip3 install pipenv
+```
+
+```cli
+git clone https://github.com/knowbee/hackmaps
+```
+
+```cli
+cd hackmaps
+```
+
+```cli
+pipenv install
+```
+
+```cli
+python run.py
+```
---
+++ app/__init__.py
@@ -0,0 +1,56 @@
+"""
+Author: Igwaneza Bruce <knowbeeinc@gmail.com>
+Attempt to collect all clinics found in rwanda,
+Source: google maps
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, WebDriverException
+from rwavalidator import isPhoneNumber
+from tqdm import tqdm
+import time
+import json
+
+def getTitle(t):
+    return t.split('\n')[0].strip()
+
+def getPhone(t):
+    phone = t.split('⋅')[-1].split('·')[-1].replace(" ", "")
+    if isPhoneNumber(phone):
+        return phone
+    else:
+        return 'No phone number'
+
+def app():
+    options = webdriver.ChromeOptions()
+
+    options.add_argument('headless')
+
+    browser = webdriver.Chrome(options=options)
+
+    data=[]
+
+    url = "https://www.google.com/search?sz=0&tbm=lcl&sxsrf=ACYBGNSiys3uXuX_0XywfYSpV74dSdepUA%3A1578507941106&ei=pR4WXq2EBsWWjLsPj7GZiAs&q=clinics+in+rwanda&oq=clinics+in+rwanda&gs_l=psy-ab.12...0.0.0.9635.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.1HfVGiEW-vU#rlfi=hd:;si:;mv:[[-1.425354,30.188343699999997],[-2.6640878,29.7141865]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2"
+    browser.get(url)
+    while True:
+        try:
+            page = browser.find_elements_by_class_name("pn")
+            clinics = browser.find_elements_by_class_name("cXedhc")
+            review_text = browser.find_elements_by_class_name(
+                "rllt__details")
+           
+            with tqdm(total=len(data)) as pbar:
+                pbar.update(len(data) / 0.1)
+                pbar.desc = 'scraping'
+                for title in clinics:
+                    data.append({'name':getTitle(title.text), 'tel':getPhone(title.text)})
+            browser.find_element_by_xpath("//*[@id='pnnext']").click()
+            time.sleep(5)
+            # WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='pnnext']")))
+        except (TimeoutException, WebDriverException) as e:
+            with open('clinics.json', 'w') as file:
+                json.dump(data, file)
+            print(f'got {len(data)} clinics in total')
+            break
